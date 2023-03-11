eb04f78adba8b9ecc7cd854c6373b532dbe6b255
shaw kunal <shawkunal3010@gmail.com>
2023-03-11 21:52:55 +0530

    completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+    const responsive ={
+        
+        0:
+        {
+            items:1
+        },
+        310:{
+            items:1
+        },
+        460:{
+            items:2
+        },
+        960:
+        {
+            items:3
+        }
+    }
 
     $nav = $('nav');
     $toggleCollapse =$('.toggle-collapse');
@@ -16,11 +33,18 @@ $(document).ready(function(){
    autoplayTimeout:3000,
    dots:false,
    nav: true,
-   navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
-
+   navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+   responsive: responsive
     });
 
+    // click to scroll up 
+    $('.move_up span').click(function(){
+        $('html,body').animate({scrollTop:0}, 2000);
+
+    })
       
+    // now make the owlcarousel responsive 
+
 
 
 });
